f9678bc519faa840318e5557bc616d94bbd71b53
João Pedro Furquim Marques <joaopedrofm@uol.com.br>
2020-04-24 01:54:36 -0300

    Create L1_Ex1.py

---
+++ L1_Ex1.py
@@ -0,0 +1,7 @@
+n1 = int(input ("Digite a primeira nota do aluno: "))
+
+n2 = int(input ("Digite a segunda nota do aluno: "))
+
+n3 = int(input ("Digite a terceira nota do aluno: "))
+
+print("Media do Aluno= {}".format(((n1 + n2 + n3) / 3)))
