a1824982c5f58b00dbc5307a67f55dca130a5443
João Pedro Furquim Marques <joaopedrofm@uol.com.br>
2020-04-24 09:40:08 -0300

    Create L1_Ex4.py

---
+++ Lista1/L1_Ex4.py
@@ -0,0 +1,3 @@
+salario = int (input("Digite o seu salario atual: R$"))
+aumento = int (input("Digite o seu percentual de aumento: "))
+print("Seu novo salario com o aumento vai ser= R${}". format((salario * ((aumento/100) + 1))))
